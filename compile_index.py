8a0898d5e7b5234dead21b12f05da2b5d9f40ccf
LivingProgram <kyhan316@gmail.com>
2017-11-22 15:59:42 -0500

    Update with alternative parser
    The alternative parser essentially treats every line as a list element
    as long as it does not have a recognizable key attached to it. Then it
    generates the corresponding yaml format, and feeds it to pyyaml to do
    the parsing. However the issue is that since pyyaml treats the lack of
    '- ' as indication of a dictionary, the first level of indents will be
    treated as dictionary and you can only have unique key values. But there
    are duplicated key needed for first level and embedded indents.

--- compile_index.py
+++ compile_index.py
@@ -1,113 +1,22 @@
 import yaml
 from pprint import pprint
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-
-def get_spaces(line):
-    return int(len(line) - len(line.lstrip(' ')))
+def convert_to_yaml(stream):
+    # the rule is: if the line does not have a known key then it will be regarded as a list element
+    yaml_string = ''
+    for line in stream:
+        if line.lstrip(' ').split(': ')[0] not in ALL_KEYS:
+            leading_spaces = len(line) - len(line.lstrip(' '))
+            yaml_string += line[:leading_spaces] + '- ' + line[leading_spaces:]
+            #print(line[:leading_spaces] + '- ' + line[leading_spaces:])
+        else:
+            yaml_string += line
+    return yaml_string
+    
 
 if __name__ == '__main__':
-    """
+    ALL_KEYS = ['title1','def','theorem','name','statement','proof'] # list of known keys
     with open("./index_data.yaml", 'r') as stream:
-        lines_dict = {}
-        for line_id, line in enumerate(stream.readlines()):
-            # compile lines_dictionary with id = line number, space = number of inital spaces, line = 'str of line'
-            num_spaces = len(line) - len(line.lstrip(' '))
-            lines_dict[line_id]= [num_spaces, line]
-        #pprint(lines_dict)
-        # generate unique list of number of initial spaces
-        spaces_list = []
-        for line_id in lines_dict:
-            spaces_list.append(lines_dict[line_id][0])
-        spaces_set = set(spaces_list)
-        # check that it has to be even, if not raise error there is an odd number
-        for num_spaces in spaces_list:
-            if num_spaces % 2 != 0:
-                raise ValueError('odd number of spaces detected in indent')
-
-        final_list = []
-        spaces_list = sorted(spaces_set) # make sure number spaces are sorted
-        spaces_list=[0] # for testing
-        for num_spaces in spaces_list: # loop over list of number of inital spaces
-            lines_list = []
-            counter = 0
-            for line_id in lines_dict:
-                if lines_dict[line_id][0] == num_spaces:
-                    if counter == 1: # if you find a line with space = number, and counter = 1, final_list.append({index:list}, index=line,list=0,counter=1
-                        final_list.append({next_index:lines_list})
-                        lines_list = []
-                    # if you find a line with space = number, add 1 to counter, set index = line
-                    counter = 1
-                    next_index = lines_dict[line_id][1]
-                else:
-                    lines_list.append(lines_dict[line_id][1])
-                    # if this is the last line, append
-                    if line_id + 1 == len(lines_dict):
-                        final_list.append({next_index:lines_list})
-        # after compiling it, change all keys with empty lists into correct format
-        #for key in final_list:
-        #    if final_list[key] == []:
-        #        new_key,data = key.split(': ')
-        #        final_list
-        #        final_list.remove(key)
-            pprint(final_list)
-                # then go into second indents, 
-    """
-    with open("./index_data.yaml", 'r') as stream:
-        saved_stream = [] # need to do this or else you cannot loop multiple times over stream
-        # generate unique list of number of initial spaces
-        spaces_list = []
-        for line in stream:
-            spaces_list.append(get_spaces(line))
-            saved_stream.append(line)
-        spaces_set = set(spaces_list)
-
-        # check that it has to be even, if not raise error there is an odd number
-        for num_spaces in spaces_list:
-            if num_spaces % 2 != 0:
-                raise ValueError('odd number of spaces detected in indent')
-
-        final_list = []
-        spaces_list = sorted(spaces_set, reverse=True) # make sure lowest 
-        spaces_list = spaces_list[1:] # remove the lowest layer
-        spaces_list = [2]
-        for num_spaces in spaces_list:
-            lines_list = []
-            counter = 0
-            line_counter = 0
-            total_lines = file_len('./index_data.yaml')
-            for line in saved_stream: # this loop is not getting run, cannot loop over stream twice?
-                line_counter += 1
-                print(line_counter)
-                if get_spaces(line) == num_spaces:
-                    if counter == 1: 
-                        if lines_list == []: # takes care of case when you do not have list
-                            key,value = next_index.split(': ')
-                            final_list.append({key:value})
-                        else:
-                            final_list.append({next_index:lines_list})
-                            lines_list = []
-                        next_index = line
-                    else:
-                        counter = 1
-                    next_index = line
-                elif get_spaces(line) < num_spaces:
-                    if 'next_index' in globals():
-                        final_list.append({next_index:lines_list}) # this break because first line triggers this
-                        lines_list = []
-                    final_list.append(line)
-                else:
-                    lines_list.append(line)
-
-                if line_counter == total_lines:
-                    if lines_list == []: # if you end on no lines list
-                        key,value = line.split(': ')
-                        final_list.append({key:value})
-                    else: # if you end on nonempty lines list
-                        final_list.append({next_index:lines_list})
-        pprint(final_list)
-                
+        data = yaml.load(convert_to_yaml(stream)) # treats the first indent as dictionary, takes only unique keys
+        # yaml.load each first indent separately 
+        pprint(data)
