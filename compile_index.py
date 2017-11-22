947c7f5898a1f8c0b657762008f8f51f0fb0d3d4
LivingProgram <kyhan316@gmail.com>
2017-11-22 16:44:44 -0500

    update with code to delete, create save lines

--- compile_index.py
+++ compile_index.py
@@ -13,4 +13,29 @@ def convert_to_yaml(stream):
 if __name__ == '__main__':
     with open("./index_data.yaml", 'r') as stream:
         data = yaml.load(convert_to_yaml(stream))
-        pprint(data)
+
+    # open html file with all its lines
+    with open("./index.html", "r") as html_file:
+        html_lines = html_file.readlines()
+
+    # delete all the lines starting from '    <div class="section">' to the very next '    </div>', there should not be any '    </div>' inbetween
+    # get the indexes of the div section for now, later remove it
+    div_start_idx = 0
+    for idx, line in enumerate(html_lines):
+        if line == '    <div class="section">\n':
+            div_start_idx = idx
+        if div_start_idx != 0:
+            if line == '    </div>\n':
+                div_end_idx = idx
+                break
+
+    # create new lines
+    new_lines = ['a','b','c']
+    
+    # insert new lines
+    html_lines = html_lines[:div_start_idx+1] + new_lines + html_lines[div_end_idx:]
+
+    # write the new list of lines to the file
+    #with open("./index.html", "w") as html_file:
+    #    html_lines = "".join(html_lines)
+    #    html_file.write(html_lines)
