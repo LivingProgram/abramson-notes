973a5b0bed2463c99abd81893e46857187ad222c
LivingProgram <kyhan316@gmail.com>
2017-11-22 16:04:22 -0500

    Update with improved method
    The new method essentially adds a '- ' in front of every line creating a
    lot of embedded lists and dictionaries, but maintaining every single
    piece of information. Later the rest of the code can deal with the
    structure of the object.

--- compile_index.py
+++ compile_index.py
@@ -2,21 +2,15 @@ import yaml
 from pprint import pprint
 
 def convert_to_yaml(stream):
-    # the rule is: if the line does not have a known key then it will be regarded as a list element
+    # insert a - in front of every line
     yaml_string = ''
     for line in stream:
-        if line.lstrip(' ').split(': ')[0] not in ALL_KEYS:
-            leading_spaces = len(line) - len(line.lstrip(' '))
-            yaml_string += line[:leading_spaces] + '- ' + line[leading_spaces:]
-            #print(line[:leading_spaces] + '- ' + line[leading_spaces:])
-        else:
-            yaml_string += line
+        leading_spaces = len(line) - len(line.lstrip(' '))
+        yaml_string += line[:leading_spaces] + '- ' + line[leading_spaces:]
     return yaml_string
     
 
 if __name__ == '__main__':
-    ALL_KEYS = ['title1','def','theorem','name','statement','proof'] # list of known keys
     with open("./index_data.yaml", 'r') as stream:
-        data = yaml.load(convert_to_yaml(stream)) # treats the first indent as dictionary, takes only unique keys
-        # yaml.load each first indent separately 
+        data = yaml.load(convert_to_yaml(stream))
         pprint(data)
